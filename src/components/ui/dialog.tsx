b1ee25e84cd670726e14b4b148b2f354ed6bb175
Vinicius Santos <vinisantoscarnero@gmail.com>
2025-07-03 12:17:37 +0000

    o erro persite, garanta que seja resolvido em todos os pop  up

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -40,7 +40,7 @@ const DialogContent = React.forwardRef<
       className={cn(
         "fixed z-50 grid w-full gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out",
         "inset-x-0 bottom-0 rounded-t-lg border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
-        "sm:w-auto sm:max-w-lg sm:rounded-lg sm:left-[50%] sm:top-[50%] sm:inset-auto sm:translate-x-[-50%] sm:translate-y-[-50%] sm:data-[state=closed]:zoom-out-95 sm:data-[state=open]:zoom-in-95 sm:data-[state=closed]:slide-out-to-left-1/2 sm:data-[state=closed]:slide-out-to-top-[48%] sm:data-[state=open]:slide-in-from-left-1/2 sm:data-[state=open]:slide-in-from-top-[48%]",
+        "sm:w-full sm:max-w-lg sm:rounded-lg sm:left-[50%] sm:top-[50%] sm:inset-auto sm:translate-x-[-50%] sm:translate-y-[-50%] sm:data-[state=closed]:zoom-out-95 sm:data-[state=open]:zoom-in-95 sm:data-[state=closed]:slide-out-to-left-1/2 sm:data-[state=closed]:slide-out-to-top-[48%] sm:data-[state=open]:slide-in-from-left-1/2 sm:data-[state=open]:slide-in-from-top-[48%]",
         className
       )}
       {...props}
